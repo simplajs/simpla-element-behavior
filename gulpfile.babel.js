52ddd925ee1d79dc442e8fa792535c5d96563f1b
Sean King <sean@seanking.org>
2017-04-17 00:59:55 +0200

    remove unecessary UMD definition

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,45 +12,16 @@ const yargs = require('yargs');
 const browserSync = require('browser-sync');
 const wct = require('web-component-tester');
 
-// HTML
-const inline = require('gulp-inline-source');
+// Processing
 const processInline = require('gulp-process-inline');
 const minify = require('gulp-htmlmin');
-
-// JS
 const eslint = require('gulp-eslint');
-const rollup = require('gulp-rollup-file');
-const resolve = require('rollup-plugin-node-resolve');
-const commonJs = require('rollup-plugin-commonjs');
-const babel = require('rollup-plugin-babel');
-const json = require('rollup-plugin-json');
-
-// CSS
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const babel = require('gulp-babel');
 
 const bs = browserSync.create(),
       argv = yargs.boolean(['debug']).argv,
       errorNotifier = () => plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }),
       OPTIONS = {
-        rollup: {
-          plugins: [
-            resolve({ main: true, browser: true }),
-            commonJs(),
-            json(),
-            babel({
-              exclude: 'node_modules/**/*'
-            })
-          ],
-          format: 'iife'
-        },
-        postcss: [
-          autoprefixer()
-        ],
-        inline: {
-          compress: false,
-          swallowErrors: true
-        },
         HTMLmin: {
           removeComments: true,
           removeCommentsFromCDATA: true,
@@ -76,27 +47,18 @@ const bs = browserSync.create(),
       };
 
 gulp.task('build', () => {
-  let styles = processInline(),
-      scripts = processInline();
+  let scripts = processInline();
 
   return gulp.src(['src/*.html'])
           .pipe(errorNotifier())
-
-          // Inline assets
-          .pipe(inline(OPTIONS.inline))
 
           // JS
           .pipe(scripts.extract('script'))
             .pipe(eslint())
             .pipe(eslint.format())
             .pipe(gulpif(!argv.debug, eslint.failAfterError()))
-            .pipe(rollup(OPTIONS.rollup))
+            .pipe(babel())
           .pipe(scripts.restore())
-
-          // CSS
-          .pipe(styles.extract('style'))
-            .pipe(postcss(OPTIONS.postcss))
-          .pipe(styles.restore())
 
           .pipe(gulpif(!argv.debug, minify(OPTIONS.HTMLmin)))
 
